81d841c28a031ce586b66cd8a2110c35b4ab8794
DangVanVy23521825 <dangvanvy112@gmail.com>
2024-07-26 14:43:35 +0700

    mainthaydoi

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-// Get the parent element where you want to duplicate the product card
-const parentElement = document.querySelector('.grid-row');
-console.log(parentElement);
-// Get the product card element you want to duplicate
-const productCard = document.querySelector('.grid-2-4-column');
+const parent = document.querySelector('.grid-row2');
+const product = document.querySelector('.grid-2-4-column');
 
-let n = 10;
-
-// Create an array of n elements (replace 'n' with the desired number of duplicates)
+let n = 19;
 const duplicates = Array(n).fill(null);
-// Loop through the array and duplicate the product card
-duplicates.forEach(() => {
-  // Clone the product card
-  const clonedProductCard = productCard.cloneNode(true);
 
-  // Append the cloned product card to the parent element
-  parentElement.appendChild(clonedProductCard);
+duplicates.forEach(() => {
+    const cloneProduct = product.cloneNode(true);
+    parent.appendChild(cloneProduct);
 });
