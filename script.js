13ddd5102a92d1aae453dafa272461e85b9d4049
DangVanVy23521825 <dangvanvy112@gmail.com>
2024-08-03 14:53:25 +0700

    mainthaydoi

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const parent = document.querySelector('.grid-row2');
-const product = document.querySelector('.grid-2-4-column');
+const parent = document.querySelector('.product__list');
+const product = document.querySelector('.product__card');
 
-let n = 19;
-const duplicates = Array(n).fill(null);
+let n = 20;
+const duplicates = Array(n - 1).fill(null);
 
 duplicates.forEach(() => {
     const cloneProduct = product.cloneNode(true);
